6bfb53a04b3279dc989980b12a10fe3b92ee3d7a
Shannon Shoemake <shannonshoemake@Shannons-MacBook-Pro.local>
2020-03-28 22:55:49 -0700

    fabfile add deploy with git code

--- README.md
+++ README.md
@@ -16,3 +16,18 @@
 
 6. Run the Project:
   python manage.py runserver
+
+
+## TODO
+1. Add search to meal screen
+2. Add "Add to cart" on ingredient detail screen
+3. Modify "Add to cart" link on ingredient screen for items that are already in cart (maybe "Add++")
+4. For cart items, add expandable bootstrap rows to show what meal an item came from
+5. Modify cart to be Week-based and not session-based, login still required
+6. Create an automated PROD deployment script (i.e. fabric/fabfile)
+   a. Backup mysql DB
+   b. Git deploy (PROD Deploy - git pull origin master --force)
+   c. Load virtual environment
+   d. install pip items
+   e. restart Apache2
+
--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,50 @@
 import getpass
+import time
+
+# from fabric.contrib.files import exists
 from fabric import task, Connection, Config
 
+# from fabric.api import cd, env, local, run
+
+REPO_URL = "https://github.com/sshoemake/Meal_Project.git"
+
+
 @task
-def hello(ctx):
-    print("Hello world!")
+def deploy(ctx):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    # print("Hello world!")
 
     sudo_pass = getpass.getpass("What's your sudo password?")
-    config = Config(overrides={'sudo': {'password': sudo_pass}})
-    c = Connection('odroid@odroidn2.local', config=config)
-    result = c.run('uname -s')
-    print(result)
+    config = Config(overrides={"sudo": {"password": sudo_pass}})
+    c = Connection("odroid@odroidn2.local", config=config)
+    result = c.run("uname -s")
+    # print(result)
 
-    whoami = c.sudo('whoami', hide='stderr')
-    print(whoami)
+    # whoami = c.sudo("whoami", hide="stderr")
+    # print(whoami)
 
-    c.run("mysqldump -u root -p'"+sudo_pass + "' meal_project > meal_project.sql")
+    c.run(
+        "mysqldump -u root -p'"
+        + sudo_pass
+        + "' meal_project > meal_project_"
+        + timestr
+        + ".sql"
+    )
 
+    site_folder = f"/home/odroid/meal_project"
+    c.run(f"mkdir -p {site_folder}")
+    with c.cd(site_folder):
+        if c.run("test -d .git", warn=True).failed:
+            c.run(f"git clone {REPO_URL} .")
+        else:
+            c.run("git fetch")
+        current_commit = c.local("git log -n 1 --format=%H", capture=True)
+        c.run(f"git reset --hard {current_commit}")
+
+
+#        if files.exists(".git"):
+#            c.run("git fetch")
+#        else:
+#            run(f"git clone {REPO_URL} .")
+#        current_commit = local("git log -n 1 --format=%H", capture=True)
+#        run(f"git reset --hard {current_commit}")
